3f4329769517d0215c23f6373ff69f5bfc852f81
mongchanghsi <m.changhsi@gmail.com>
2022-02-15 23:45:55 +0800

    Update featured works content

--- utils/constants/featured.ts
+++ utils/constants/featured.ts
@@ -1,8 +1,7 @@
 import SutdReviewImage from '../../public/assets/featured/sutdreview.png';
-import RRCEvoImage from '../../public/assets/featured/rrc_evo.png';
-import RRCMintImage from '../../public/assets/featured/rrc_mint.png';
 import MintWebsitesImage from '../../public/assets/featured/mint_websites.png';
 import NamecardImage from '../../public/assets/featured/namecard.png';
+import IdentityPalImage from '../../public/assets/featured/identitypal.png';
 
 const featured = [
   {
@@ -19,13 +18,20 @@ const featured = [
     description:
       'Developed minting websites for clients. Click here to visit my dApps portfolio.',
   },
-  // {
-  //   title: 'Open-Source Namecard Website',
-  //   image: NamecardImage,
-  //   url: 'https://ericmong-namecard.netlify.app/',
-  //   description:
-  //     'An open-source namecard website which displays all your socials and even generate a QR code to allow your friends to scan to the same page.',
-  // },
+  {
+    title: 'Open-Source Namecard Website',
+    image: NamecardImage,
+    url: 'https://ericmong-namecard.netlify.app/',
+    description:
+      'An open-source namecard website which displays all your socials, generate a QR code to allow your friends to scan to the same page and to directly save the contract to your phonebook',
+  },
+  {
+    title: 'IdentityPal - Capstone Project with PayPal',
+    image: IdentityPalImage,
+    url: '',
+    description:
+      "Kiosk frontend to allow hotel customers to perform contactless check in either through conventional passport or proposed PayPal's own KYC method",
+  },
 ];
 
 export default featured;
