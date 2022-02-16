0b884f57dc4c701c029a700af07a1477e25cf0c8
mongchanghsi <m.changhsi@gmail.com>
2022-02-16 22:02:48 +0800

    Fix viewing in mobile mde

--- components/FeaturedWorks.tsx
+++ components/FeaturedWorks.tsx
@@ -1,19 +1,46 @@
 /* eslint-disable @next/next/no-img-element */
-import { useRef, useEffect, MutableRefObject, useState } from 'react';
+import {
+  useRef,
+  useEffect,
+  MutableRefObject,
+  useState,
+  useCallback,
+} from 'react';
 import styles from '../styles/FeaturedWorks.module.scss';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import featured from '../utils/constants/featured';
 import gsap from 'gsap';
+import useWindowDimensions from '../hooks/useWindowDimension';
+
+const MEDIUM_SCREEN_SIZE = 768;
 
 const FeaturedWorks = () => {
+  const { windowDimensions } = useWindowDimensions();
+
   const el = useRef() as MutableRefObject<HTMLDivElement>;
   const q = gsap.utils.selector(el);
   const tl = useRef<gsap.core.Timeline>();
 
   const [mainIndex, setMainIndex] = useState<number>(0);
 
-  const items = featured.map((item, index: number) => {
+  const isMediumScreen = useCallback(() => {
+    return windowDimensions?.width < MEDIUM_SCREEN_SIZE;
+  }, [windowDimensions]);
+
+  const itemsDisplayInMediumScreen = featured.map((item, index: number) => {
+    return (
+      <div key={index} className={styles.item}>
+        <img
+          src={item.image.src}
+          alt={item.title}
+          className={styles.item_image}
+        />
+      </div>
+    );
+  });
+
+  const itemsDisplayInLargeScreen = featured.map((item, index: number) => {
     return (
       <div
         key={index}
@@ -88,28 +115,81 @@ const FeaturedWorks = () => {
       </div>
 
       <div className={styles.content} id='FeaturedWorksContent'>
-        <div
-          className={`${styles.controller} unselectable`}
-          onClick={slidePrev}
-        >
-          &lang;
-        </div>
-        <div className={styles.carousel}>
-          <AliceCarousel
-            activeIndex={mainIndex}
-            animationDuration={800}
-            animationType='fadeout'
-            disableDotsControls
-            disableButtonsControls
-            items={items}
-          />
-        </div>
-        <div
-          className={`${styles.controller} unselectable`}
-          onClick={slideNext}
-        >
-          &rang;
-        </div>
+        {isMediumScreen() ? (
+          <>
+            <div className={styles.content_col}>
+              <div className={styles.content}>
+                <div
+                  className={`${styles.controller} unselectable`}
+                  onClick={slidePrev}
+                >
+                  &lang;
+                </div>
+                <div className={styles.carousel}>
+                  <AliceCarousel
+                    activeIndex={mainIndex}
+                    animationDuration={800}
+                    animationType='fadeout'
+                    disableDotsControls
+                    disableButtonsControls
+                    items={itemsDisplayInMediumScreen}
+                  />
+                </div>
+                <div
+                  className={`${styles.controller} unselectable`}
+                  onClick={slideNext}
+                >
+                  &rang;
+                </div>
+              </div>
+
+              <div
+                id={`WorksContent_${featured[mainIndex].title}`}
+                className={styles.item_textContent}
+              >
+                <h3 className={styles.item_title}>
+                  {featured[mainIndex].title}{' '}
+                  <a
+                    href={featured[mainIndex].url}
+                    className={styles.item_small}
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    View Project
+                  </a>
+                </h3>
+                <p className={styles.item_text}>
+                  {featured[mainIndex].description}
+                </p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className={`${styles.controller} unselectable`}
+              onClick={slidePrev}
+            >
+              &lang;
+            </div>
+            <div className={styles.carousel}>
+              <AliceCarousel
+                activeIndex={mainIndex}
+                animationDuration={800}
+                animationType='fadeout'
+                disableDotsControls
+                disableButtonsControls
+                items={itemsDisplayInLargeScreen}
+              />
+            </div>
+            <div
+              className={`${styles.controller} unselectable`}
+              onClick={slideNext}
+            >
+              &rang;
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
